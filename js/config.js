ced87393f5671f97617ea0cbb0f0260e5a1e21b7
pharmachris <pharm.tsd@gmail.com>
2026-08-21 17:08:33 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // js/config.js
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbwEqCMNLdz9Aw_b1oHeEbT5ovrF1RslfJjyyssZU0TJiTXxvq9qUP2I6Cx8BoPIJTDzyg/exec";
+const GAS_API_URL = "https://script.google.com/macros/library/d/1CqQsYHzH_Cac_7S2wWhQIAk1M0O_eM9J3C5UZvHq3T3O8hIcb60DPlOq/2";
