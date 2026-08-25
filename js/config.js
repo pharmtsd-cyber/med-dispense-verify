35d9383030c596bdc7268579884f46455bdad8ec
pharmachris <pharm.tsd@gmail.com>
2026-08-25 15:10:15 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,5 @@
 // js/config.js
 const GAS_API_URL = "https://script.google.com/macros/s/AKfycbwEqCMNLdz9Aw_b1oHeEbT5ovrF1RslfJjyyssZU0TJiTXxvq9qUP2I6Cx8BoPIJTDzyg/exec";
+
+// 👉 您可以在這裡自由調整系統開機時，要往回撈取幾天的紀錄 (設定 0 則代表不限制，全部載入)
+const LOAD_HISTORY_DAYS = 90;
